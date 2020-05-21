aacf64de8b0eb87393657e4008449c983043fe0d
András Tóth <csonti04@example.com>
2020-05-21 22:31:13 +0200

    Add project files.

---
+++ ASAP_WPF/ImageProcessor.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace ASAP_WPF
+{
+    class ImageProcessor :BaseThread
+    {
+        public ImageProcessor()
+            : base()
+        {
+        }
+
+        public override void RunThread()
+        {
+            Process();
+        }
+        private int AdaptiveThresholdConstant { get; set; }
+
+        private Mat ImageMat { get; set; }
+
+        public void SetValues(Mat _imgMat, int _const)
+        {
+            this.ImageMat = _imgMat;
+            this.AdaptiveThresholdConstant = _const;
+        }
+
+        public void SetValues(string _imgPath, int _const)
+        {
+            this.ImageMat = CvInvoke.Imread(_imgPath);
+            this.AdaptiveThresholdConstant = _const;
+        }
+
+        public void Process()
+        {
+            if(null == this.ImageMat)
+            {
+                return;
+            }
+
+
+            //Itt megkellene nézni, hogy mik vannak minden enum mögött, hogy van-e jobb alternatíva
+            //Blurring image
+            var tempSize = new Size(5,5);
+            //Using input | output arrays instead of temp objects
+            CvInvoke.GaussianBlur(this.ImageMat, this.ImageMat, tempSize, 0);
+            //Adaptive threshold
+            var tempAdaptiveThreshold = this.AdaptiveThresholdConstant;
+            //Azt az 59-et majd meg kell lesni, hogy miért az van
+            CvInvoke.AdaptiveThreshold(this.ImageMat, this.ImageMat,255, Emgu.CV.CvEnum.AdaptiveThresholdType.MeanC, Emgu.CV.CvEnum.ThresholdType.BinaryInv,59, this.AdaptiveThresholdConstant);
+            //Get contours and remove small patches
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat hierarchy = new Mat();
+            // meg kellene nézni miez ez a mat, vactorofvectorpoint és input, output és inoutputarrayek
+            CvInvoke.FindContours(this.ImageMat, contours, hierarchy,Emgu.CV.CvEnum.RetrType.List,Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+
+
+            var dims = contours.Size;
+            for (var idx = 0; idx < dims ; idx++)
+            {
+                var con = contours[idx];
+                if (CvInvoke.ContourArea(con) < 800)
+                {
+                    MCvScalar color = new MCvScalar(0,0,0);
+                    CvInvoke.FillPoly(this.ImageMat, con, color);
+                }
+            }
+            //Open then close to close gaps
+            var kernelMat1 = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle, new Size(3, 3), new Point(-1, -1));
+            CvInvoke.MorphologyEx(
+                this.ImageMat,
+                this.ImageMat,
+                Emgu.CV.CvEnum.MorphOp.Open,
+                kernelMat1,
+                new Point(-1, -1),
+                1 ,Emgu.CV.CvEnum.BorderType.Default,
+                new MCvScalar());
+            var kernelMat2 = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle, new Size(7, 7), new Point(-1, -1));
+            CvInvoke.MorphologyEx(
+                this.ImageMat,
+                this.ImageMat,
+                Emgu.CV.CvEnum.MorphOp.Close,
+                kernelMat2,
+                new Point(-1, -1),
+                2,
+                Emgu.CV.CvEnum.BorderType.Default,
+                new MCvScalar());
+            //Get new contours find the correct ones by size and hierarchy draw them onto the img, draw bounding box, and length
+            CvInvoke.FindContours(this.ImageMat,contours, hierarchy, Emgu.CV.CvEnum.RetrType.Tree, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            //Empty out objects contours
+            var contourToReturn = new VectorOfVectorOfPoint();
+            //Image to be overlayed, with alpha values
+            Mat contourImage = new Mat(this.ImageMat.Rows,this.ImageMat.Cols, Emgu.CV.CvEnum.DepthType.Cv8U,4);
+
+            dims = contours.Size;
+            for (var idx = 0; idx < dims; idx++)
+            {
+                var con = contours[idx];
+                //Skip small patches
+                if (CvInvoke.ContourArea(con) < 300)
+                {
+                    continue;
+                }
+                //Only use ones that have parents, so the inner ones
+                //https://stackoverflow.com/questions/41560048/c-sharp-emgu-cv-findcontours-hierarchy-data-always-null
+                //https://stackoverflow.com/questions/37408481/navigate-through-hierarchy-of-contours-found-by-findcontours-method/37470968#37470968
+                //http://www.emgu.com/forum/viewtopic.php?t=6333
+                //http://www.emgu.com/forum/viewtopic.php?f=7&t=5263
+                //https://stackoverrun.com/fr/q/11437249
+                //Hát bazdmeg erre sosem jöttem volna rá
+                if ((int)hierarchy.GetData().GetValue(0,idx,3) < 0)
+                {
+                    continue;
+                }
+                contourToReturn.Push(con);
+                CvInvoke.DrawContours(contourImage,contours,0,new MCvScalar(0,255,0,255),2);
+
+                var rect = CvInvoke.MinAreaRect(con);
+                var box = CvInvoke.BoxPoints(rect);
+                VectorOfPointF boxVec = new VectorOfPointF(box);
+                //az jókérdés, hogy ez most a megfelelő alak-e vagy sem
+                CvInvoke.DrawContours(contourImage, boxVec,0,new MCvScalar(0, 255, 0, 255), 2);
+            }
+
+            CvInvoke.PutText(this.ImageMat, "{}", new Point(100,300),Emgu.CV.CvEnum.FontFace.HersheySimplex,8.0,new MCvScalar(255),5 );
+
+            //itt kellene visszaadni a szálat!
+        }
+
+    }
+}
---
+++ ASAP_WPF/Models.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Microsoft.VisualBasic.CompilerServices;
+using System.Threading;
+using System.Windows;
+using Emgu.CV.Structure;
+using Point = System.Drawing.Point;
+
+namespace ASAP_WPF
+{
+    class Models
+    {
+        private string FolderName { get; set; }
+        private string ImgName { get; set; }
+        private List<String> Files { get; set; } //paths for the time being
+        private int OpenedImgNumber { get; set; }
+        private Mat Image { get; set; }
+        private Mat ProcessedImage { get; set; }
+        private List<VectorOfVectorOfPoint> Contours { get; set; }
+        private Mat CountourImage { get; set; }
+
+        private int AdaptiveThresholdConstant { get; set; }
+        //sginals? https://softwareengineering.stackexchange.com/questions/142458/any-practical-alternative-to-the-signals-slots-model-for-gui-programming
+        //threadpool System.Threading.ThreadPool;
+
+        private ImageProcessor ImgProcessor { get; set; } //auto delete?? signalos baszás itt is
+
+        public override string ToString()
+        {
+            return "ImageHandler, opened img:" + this.ImgName;
+        }
+
+        public Models()
+        {
+            FolderName = null;
+            ImgName = null;
+            Files = new List<string>();
+            OpenedImgNumber = 0;
+            Image = null;
+            ProcessedImage = null;
+            Contours = new List<VectorOfVectorOfPoint>();
+            CountourImage = null;
+            AdaptiveThresholdConstant = 5;
+            ImgProcessor = new ImageProcessor();
+        }
+
+        public void UpdateImage()
+        {
+            if (this.Files.Count <= 0 || this.OpenedImgNumber < 0) return;
+            this.ImgName = this.Files[OpenedImgNumber];
+            //itt lehetne kísérletezni, hogy egyből két árnyalatos szürkével menjen, vagy a hisztogram generáláshoz több árnyalatos legyen
+            this.Image = CvInvoke.Imread(this.ImgName, Emgu.CV.CvEnum.ImreadModes.ReducedGrayscale8);
+            this.ImgProcessor.SetValues(this.Image, this.AdaptiveThresholdConstant);
+            //this.isloading == ture???? ez valami signalos lehet
+            //this.signals.image_provcessing_change.emit(True)
+        }
+
+        public void ProcessProcessedThread(List<object> _returnedList)
+        {
+            this.ProcessedImage = (Mat) _returnedList[0];
+            this.Contours = (List<VectorOfVectorOfPoint>) _returnedList[1];
+            this.CountourImage = (Mat) _returnedList[2];
+
+            //this.loading = False
+            //this.signals.image_processing_change.emit(False)
+        }
+
+        public void UpdateFolder(String _path)
+        {
+            var attr = File.GetAttributes(_path);
+            if (null == _path && (attr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                return;
+            }
+
+            this.FolderName = _path;
+            this.Files = new List<string>(Directory.GetFiles(_path)).OrderBy(q => q).ToList();
+            this.OpenedImgNumber = 0;
+            this.UpdateImage();
+        }
+
+        public void NextImage()
+        {
+            //if is loading return??? ezekre tényleg meg kell nézni a dolgokat
+            if (this.OpenedImgNumber + 1 > this.Files.Count)
+            {
+                this.OpenedImgNumber++;
+            }
+
+            this.UpdateImage();
+        }
+
+        public void PreviousImage()
+        {
+            //if is loading return??? ezekre tényleg meg kell nézni a dolgokat
+            if (this.OpenedImgNumber - 1 >= 0)
+            {
+                this.OpenedImgNumber--;
+            }
+
+            this.UpdateImage();
+        }
+
+        public void
+            SetAdaptiveThresholdConstant(
+                int _value) //http://www.learncsharptutorial.com/threadpooling-csharp-example.php
+        {
+            if (this.AdaptiveThresholdConstant == _value)
+            {
+                return;
+            }
+
+            this.AdaptiveThresholdConstant = _value;
+            //this.loading = True
+            //this.signals.image_processing_change.emit(True)
+            //ThreadPool.QueueUserWorkItem(this.ImgProcessor);
+            this.ImgProcessor.RunThread();
+        }
+
+        public void JumpToImage(int _idx)
+        {
+            //If this is loading return????
+            if (_idx < 0 || _idx >= this.Files.Count) return;
+            this.OpenedImgNumber = _idx;
+            UpdateImage();
+        }
+
+        public void DrawCellCountours(PointF _point)
+        {
+            foreach (var tempRect in from contour in Contours
+                where CvInvoke.PointPolygonTest(contour, _point, true) >= 0
+                select CvInvoke.MinAreaRect(contour))
+            {
+                PointF[] box = CvInvoke.BoxPoints(tempRect);
+                VectorOfPointF boxVec = new VectorOfPointF(box);
+                CvInvoke.PutText(CountourImage, tempRect.Size.Height.ToString(),
+                    new System.Drawing.Point((int) (10 + box[0].X), (int) (10 + box[0].Y)),
+                    Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.65, new MCvScalar(255, 100, 100, 255), 2);
+            }
+        }
+
+        //Returns the length of a cell around a point
+        public double GetCellLength(PointF _point)
+        {
+            foreach (var tempRect in from contour in Contours
+                where CvInvoke.PointPolygonTest(contour, _point, true) >= 0
+                select CvInvoke.MinAreaRect(contour))
+            {
+                //ezek valszeg nem kellenek ide
+                //PointF[] box = CvInvoke.BoxPoints(tempRect);
+                //VectorOfPointF boxVec = new VectorOfPointF(box);
+                return tempRect.Size.Height;
+            }
+
+            return -1.0;
+        }
+
+        public void SaveImgWithCountours(PointF _point)
+        {
+            Mat ImgToSave = this.Image;
+            foreach (var contour in Contours)
+            {
+                CvInvoke.DrawContours(ImgToSave, contour, 0, new MCvScalar(0, 255, 0), 2);
+                RotatedRect tempRect = CvInvoke.MinAreaRect(contour);
+                PointF[] box = CvInvoke.BoxPoints(tempRect);
+                VectorOfPointF boxVec = new VectorOfPointF(box);
+                CvInvoke.DrawContours(ImgToSave, boxVec, 0, new MCvScalar(0, 0, 255), 2);
+                if (CvInvoke.PointPolygonTest(contour, _point, true) >= 0)
+                {
+                    CvInvoke.PutText(CountourImage, tempRect.Size.Height.ToString(),
+                        new System.Drawing.Point((int) (10 + box[0].X), (int) (10 + box[0].Y)),
+                        Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.65, new MCvScalar(255, 100, 100, 255), 2);
+                }
+            }
+
+            String processedDir = this.FolderName + Path.DirectorySeparatorChar + "processed";
+            if (!Directory.Exists(processedDir))
+            {
+                Directory.CreateDirectory(processedDir);
+            }
+
+            CvInvoke.Imwrite(processedDir + Path.DirectorySeparatorChar + this.OpenedImgNumber + ".jpg", ImgToSave);
+        }
+
+        public Point ContourCenter(PointF _point)
+        {
+            Point temp = new Point();
+            foreach (var contour in Contours)
+            {
+                if (!(CvInvoke.PointPolygonTest(contour, _point, true) >= 0)) continue;
+                var moment = CvInvoke.Moments(contour);
+                var cx = moment.M10 / moment.M00;
+                var cy = moment.M01 / moment.M00;
+                temp = new Point((int) cx, (int) cy);
+                break;
+            }
+
+            return temp;
+        }
+    }
+}
