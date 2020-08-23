5e9ce2e460f5d551be7a4c4c46743c345f012a53
András Tóth <csonti04@example.com>
2020-08-23 15:05:40 +0200

    Added to diffraction point detection work.

--- ASAP_WPF/ExtensionMethods.cs
+++ ASAP_WPF/ExtensionMethods.cs
@@ -42,8 +42,20 @@ namespace ASAP_WPF
             return vectorToReturn;
         }
 
+        public static Mat RotateMatWithoutCutoff(this Mat matToRotate, double angle)
+        {
+            var matToReturn = matToRotate.CreateNewHardCopyFromMat();
+            //TODO befelyezni :|
+            //https://www.pyimagesearch.com/2017/01/02/rotate-images-correctly-with-opencv-and-python/
+            return matToReturn;
+        }
+
         public static (Point, Point) GetPointsOfWidestSliceOfCell(this Mat matToMeasure, Point cornerA, Point cornerB)
-        {//https://jayrambhia.wordpress.com/2012/09/20/roi-bounding-box-selection-of-mat-images-in-opencv/
+        {
+            //Kell majd hozzá a bounding box
+            //https://stackoverflow.com/questions/15043152/rotate-opencv-matrix-by-90-180-270-degrees
+            //https://jayrambhia.wordpress.com/2012/09/20/roi-bounding-box-selection-of-mat-images-in-opencv/
+            //https://www.pyimagesearch.com/2017/01/02/rotate-images-correctly-with-opencv-and-python/
             //var roiMat = matToMeasure.CreateNewHardCopyFromMat();
             var tempA = new Point(0);
             var tempB = new Point(0);
@@ -54,29 +66,55 @@ namespace ASAP_WPF
 
             //roiMat = roiMat(roiRectangle);
 
+            var biggestAreaWindow = GetBiggestAreaOfCellWithSlidingWindow(roiMat, 5);
+            var biggestAreaRow = GetBiggestAreaOfCellWithSlidingWindow(biggestAreaWindow, 1);
+            var (firstOffset, secondOffset) = biggestAreaRow.GetCenterIdxOfDiffractionLineSlice();
+
             return (tempA, tempB);
         }
 
-        public static Mat GetWidestSliceOfCell(this Mat matToMeasure)
+        /*public static Mat GetWidestSliceOfCell(this Mat matToMeasure)
         {
             var returnMat = new Mat();
 
 
 
             return returnMat;
+        }*/
+
+        public static (int, int) GetCenterIdxOfDiffractionLineSlice(this Mat matToMeasure)
+        {
+            var firstSlice = new List<int>();
+            var secondSlice = new List<int>();
+            var firstWhiteFound = false;
+            var firstBlackAfterFirstSlice = false;
+
+            if(matToMeasure.Rows > 1) throw new Exception("Given Mat contains more than on row");
+            var tempRow = matToMeasure.Row(0);
+            for (var colIdx = 0; colIdx < matToMeasure.Cols; colIdx++)
+            {
+                var tempValue = (int)tempRow.GetData().GetValue(0, colIdx, 0);
+                if (0 != tempValue) continue;
+                if (!firstWhiteFound) firstWhiteFound = true;
+                firstSlice.Add(colIdx);
+                if (!firstWhiteFound) continue;
+                if (!firstBlackAfterFirstSlice && 255 == tempValue) firstBlackAfterFirstSlice = true;
+                if(firstBlackAfterFirstSlice && 0 == tempValue) secondSlice.Add(colIdx);
+            }
+            return (firstSlice[firstSlice.Count / 2], secondSlice[secondSlice.Count / 2]);
         }
 
-        public static (Mat, Mat) SliceMatInHalf(this Mat matToSlice)
+        public static (Mat, Mat) SliceMatInHalfHorizontally(this Mat matToSlice)
         {//https://answers.opencv.org/question/82641/dividing-image-horizontally-into-equal-parts/
 
-            if(matToSlice.Height < 2) throw new Exception("Given matrix has 1 or no rows!");
+            if(matToSlice.Rows < 2) throw new Exception("Given matrix has 1 or no rows!");
             var upperHalfRectFirstPoint = new Point(0,0);
-            var upperHalfRectSecondPoint = new Point(matToSlice.Cols, matToSlice.Height / 2);
+            var upperHalfRectSecondPoint = new Point(matToSlice.Cols, matToSlice.Rows / 2);
             var firstTempSize = new Size(upperHalfRectSecondPoint.X - upperHalfRectFirstPoint.X, upperHalfRectSecondPoint.Y - upperHalfRectFirstPoint.Y);
             var upperHalfRect = new Rectangle(upperHalfRectFirstPoint, firstTempSize);
 
             var lowerHalfRectFirstPoint = new Point(0, upperHalfRect.Height);
-            var lowerHalfRectSecondPoint = new Point(matToSlice.Cols, matToSlice.Height - upperHalfRect.Height);
+            var lowerHalfRectSecondPoint = new Point(matToSlice.Cols, matToSlice.Rows - upperHalfRect.Height);
             var secondTempSize = new Size(lowerHalfRectSecondPoint.X - lowerHalfRectFirstPoint.X, lowerHalfRectSecondPoint.Y - lowerHalfRectFirstPoint.Y);
             var lowerHalfRect = new Rectangle(lowerHalfRectFirstPoint, secondTempSize);
 
@@ -88,6 +126,44 @@ namespace ASAP_WPF
             if(upperHalf.Height + upperHalf.Height != matToSlice.Height) throw new Exception("Separated halves summed height does not equal the original matrix height!");
 
             return (upperHalf, lowerHalf);
+        }
+
+        public static (Mat, Mat) SliceMatInHalfHorizontallyUsingRange(this Mat matToSlice)
+        {
+            var upperHalf = GetRowsFromRange(matToSlice,0, matToSlice.Rows / 2);
+            var lowerHalf = GetRowsFromRange(matToSlice, matToSlice.Rows - upperHalf.Height, matToSlice.Rows);
+            return (upperHalf, lowerHalf);
+        }
+
+        public static (Mat, Mat) SliceMatInHalfVerticallyUsingRange(this Mat matToSlice)
+        {
+            var upperHalf = GetColsFromRange(matToSlice, 0, matToSlice.Cols / 2);
+            var lowerHalf = GetColsFromRange(matToSlice, matToSlice.Cols - upperHalf.Width, matToSlice.Cols);
+            return (upperHalf, lowerHalf);
+        }
+
+        public static (Mat, Mat) SliceMatInHalfVertically(this Mat matToSlice)
+        {//https://answers.opencv.org/question/82641/dividing-image-horizontally-into-equal-parts/
+            //leget ezt is range-gel kellene?
+            if (matToSlice.Cols < 2) throw new Exception("Given matrix has 1 or no rows!");
+            var leftHalfRectFirstPoint = new Point(0, 0);
+            var leftHalfRectSecondPoint = new Point(matToSlice.Cols / 2, matToSlice.Height );
+            var firstTempSize = new Size(leftHalfRectSecondPoint.X - leftHalfRectFirstPoint.X, leftHalfRectSecondPoint.Y - leftHalfRectFirstPoint.Y);
+            var leftHalfRect = new Rectangle(leftHalfRectFirstPoint, firstTempSize);
+
+            var rightHalfRectFirstPoint = new Point(0, leftHalfRect.Width);
+            var rightHalfRectSecondPoint = new Point(matToSlice.Cols, matToSlice.Cols - leftHalfRect.Width);
+            var secondTempSize = new Size(rightHalfRectSecondPoint.X - rightHalfRectFirstPoint.X, rightHalfRectSecondPoint.Y - rightHalfRectFirstPoint.Y);
+            var rightHalfRect = new Rectangle(rightHalfRectFirstPoint, secondTempSize);
+
+            var leftHalf = new Mat(matToSlice, leftHalfRect);
+            var rightHalf = new Mat(matToSlice, rightHalfRect);
+
+            //Talán kellene a RowRange, mert az o1
+
+            if (leftHalf.Height + leftHalf.Height != matToSlice.Height) throw new Exception("Separated halves summed height does not equal the original matrix height!");
+
+            return (leftHalf, rightHalf);
         }
 
         //Majd a forgatáshoz
@@ -103,7 +179,7 @@ namespace ASAP_WPF
             var prevPixelVal = -1;
 
             if(matToMeasure.Dims > 1) throw new Exception("Given matrix does not contain a greyscale picture!");
-            var tempArray = matToMeasure.Data;
+            //var tempArray = matToMeasure.Data;
             /*
             for (var colIdx = 0; colIdx < matToMeasure.Cols; colIdx++)
 
@@ -177,7 +253,13 @@ namespace ASAP_WPF
             return returnMat;
         }
 
-
+        public static Mat GetColsFromRange(this Mat matToSlice, int colStartIDx, int colEndIdx)
+        {
+            var rowRange = new Range(0, matToSlice.Rows);
+            var colRange = new Range(colStartIDx, colEndIdx);
+            var returnMat = new Mat(matToSlice, rowRange, colRange);
+            return returnMat;
+        }
 
         public static int GetPrevValueOfMatrix(this Mat matToMeasure, int colIdx, int rowIdx)
         {
